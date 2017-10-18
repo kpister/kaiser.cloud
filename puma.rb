808166228087a419853f2dfd8f00a6ee01e96c5c
Kaiser Pister <pisterk@gmail.com>
2017-10-18 12:27:55 -0700

    prevent sequel connection problems?

--- puma.rb
+++ puma.rb
@@ -1,3 +1,4 @@
+require 'sequel'
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
@@ -10,4 +11,6 @@ environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
     # connect to database?
+    Sequel.extension :connection_validator
+    Sequel.pool.connection_validation_timeout = -1
 end
