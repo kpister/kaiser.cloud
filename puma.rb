3a19ba730edc2a302e63c5603466c4d5270faa98
Kaiser Pister <pisterk@gmail.com>
2017-10-18 12:30:57 -0700

    revert change, look for another?

--- puma.rb
+++ puma.rb
@@ -1,4 +1,3 @@
-require 'sequel'
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
@@ -10,7 +9,5 @@ port        ENV['PORT']     || 9292
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-    # connect to database?
-    Sequel.extension :connection_validator
-    Sequel.pool.connection_validation_timeout = -1
+    require_relative 'db'
 end
